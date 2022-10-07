eeb2533eee5d24f574cafe2c3f7cf7c48533ca47
Plamer <plamer88@yandex.ru>
2022-10-07 13:47:22 +0300

    add 404 and 500 error handler

--- run.py
+++ run.py
@@ -12,5 +12,22 @@ app.register_blueprint(api_blueprint)
 app.register_blueprint(bookmarks_blueprint)
 app.register_blueprint(main_blueprint)
 
+
+@app.errorhandler(404)
+def error_404(error_code):
+
+    print(f'Возникла ошибка {error_code}')
+
+    return 'Упс, страница, которую вы искали не существует, код ошибки - 404'
+
+
+@app.errorhandler(500)
+def error_500(error_code):
+
+    print(f'Возникла ошибка {error_code}')
+
+    return 'Возникла ошибка со стороны сервера, код ошибки - 500'
+
+
 if __name__ == '__main__':
     app.run()
